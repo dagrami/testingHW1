683f95e0ffeeb2ea0b48bd3280e04b3e6ca18371
Daniel Miller <dagrami@gmail.com>
2013-01-21 08:13:32 -0500

    removed warning

--- TestingHW1/src/TTTGraphics2PTest.java
+++ TestingHW1/src/TTTGraphics2PTest.java
@@ -1,6 +1,5 @@
-import static org.junit.Assert.*;
-
-import javax.swing.SwingUtilities;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.After;
 import org.junit.Before;
